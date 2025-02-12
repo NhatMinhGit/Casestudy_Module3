85703dd1c84d63717a9a39b263df78c77c392af7
Nguyễn Trần Nhật Minh <92421541+NhatMinhGit@users.noreply.github.com>
2025-02-12 17:13:12 +0700

    Update HelloServlet.java

--- src/main/java/org/example/websitekinhdoanhpc_casestudy_module3/HelloServlet.java
+++ src/main/java/org/example/websitekinhdoanhpc_casestudy_module3/HelloServlet.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.example.websitekinhdoanhpc_casestudy_module3;
 
 import java.io.*;
@@ -26,33 +25,4 @@ public class HelloServlet extends HttpServlet {
 
     public void destroy() {
     }
-=======
-package org.example.websitekinhdoanhpc_casestudy_module3;
-
-import java.io.*;
-
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
-
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
->>>>>>> NguyenTranNhatMinh
 }
